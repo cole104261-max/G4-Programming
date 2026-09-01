af3d02a073ed522e4e00dc924d5ec7ad016a0c29
cole104261 <cole104261@mcpsva.org>
2026-09-01 15:08:59 -0400

    Send

--- interview.py
+++ interview.py
@@ -1,11 +1,17 @@
+"""
+Filename: interview.py
+Author: <Schuyler, Cole>
+Created: <9/1/2026>
+Instructor: Burgess
+"""
 
 
 
 
 x = input("are you over 18?")
-x = input("have you worked yet?")
-input("may I see your resume?")
-input("what are your strengths?")
-input("what are your weaknesses?")
+y = input("have you worked yet?")
+v = input("may I see your resume?")
+z = input("what are your strengths?")
+w = input("what are your weaknesses?")
 
-print(x)
+print(x, y, z, v, w)
