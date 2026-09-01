f0ca02c709575edce6e1f14b156826d6b9e2973c
cole104261 <cole104261@mcpsva.org>
2026-09-01 14:57:07 -0400

    Send

---
+++ interview.py
@@ -0,0 +1,11 @@
+
+
+
+
+x = input("are you over 18?")
+x = input("have you worked yet?")
+input("may I see your resume?")
+input("what are your strengths?")
+input("what are your weaknesses?")
+
+print(x)
